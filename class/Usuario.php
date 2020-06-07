68b01e2efcb9cdeb1f4d38589d13278a28edec1b
Guilherme <gdoliveirasouza@gmail.com>
2020-06-07 12:27:09 -0300

    atualizando projeto

--- class/Usuario.php
+++ class/Usuario.php
@@ -7,6 +7,12 @@ class Usuario{
 	private $Senha;
 	private $DataCadastro;
 
+// Atribui vazio ("") nos atributos pra não afetar quando eu instanciar uma classe sem parametros no construtor 
+	public function __construct($pNome = "", $pSenha = ""){  
+		$this->setNome($pNome);
+		$this->setSenha($pSenha);
+	}
+
 
 	public function getID(){
 		return $this->ID;
@@ -54,24 +60,106 @@ class Usuario{
 
 			$row = $result[0];
 
-			$this-> setID($row['ID']);
-			$this-> setNome($row['Nome']);
-			$this-> setSenha($row['Senha']);
-			$this-> setDataCadastro(new dateTime($row['Data_Cadastro']));
+			$this->setData($result[0]);
 			}
 		}
 
-		public function __toString(){
+	public static function listarUsuarios(){
 
-			return json_encode(array(
-				"ID"=>$this->getID(),
-				"Nome"=>$this->getNome(),
-				"Senha"=>$this->getSenha(),
-				"DataCastro"=>$this->getDataCasdatro()->format("d/m/Y H:i:s")
-			));
+		$sql = new Sql();
+			return $sql->select("Select * From tbl_usuario order by nome");
 
+	}
+
+	public static function search($nome){
+		$sql = new Sql();
+
+		return $sql->select ("Select * From tbl_usuario where nome like :SEARCH order by nome",array(
+			":SEARCH"=>"%".$nome."%"));
+	}
+
+	public function login($nome,$senha){
+
+		$sql = new Sql();
+
+		$result = $sql->select("Select * From tbl_usuario Where Nome = :Nome and Senha = :Senha", array(
+			":Nome"=>$nome,
+			":Senha"=>$senha));
+
+			if (isset($result[0])){
+
+			$row = $result[0];
+
+			$this->setData($result[0]);
+
+			}
+			else{
+				throw new Exception ("logind e senha invalidos");
+			} 
+	}
+	public function setData($data){ 
+
+		$this-> setID($data['ID']);
+		$this-> setNome($data['Nome']);
+		$this-> setSenha($data['Senha']);
+		$this-> setDataCadastro(new DateTime($data['Data_Cadastro']));		
+	}
+
+	public function alterar($Nome, $Senha){
+		$this->setNome($Nome);
+		$this->setSenha($Senha);
+
+		$sql = new Sql();
+		$sql->query("UPDATE tbl_usuario SET Nome = :NOME, Senha = :SENHA Where ID = :ID",array(
+			':NOME'=>$this->getNome(),
+			':SENHA'=>$this->getSenha(),
+			':ID'=>$this->getID()
+		));
+	}
+
+	public function excluir(){
+		$sql = new Sql();
+		$sql->query("DELETE from tbl_usuario where ID = :ID", array(
+			':ID'=>$this->getID()
+		));
+
+		$this->setID(0);
+		$this->setNome("");
+		$this->setSenha("");
+		$this->setDataCadastro(new DateTime(""));
+	}
+
+	public function incluir (){
+
+		$sql =  new Sql();
+		$result = $sql->select("Call sp_usuarios_insert(:NOME, :SENHA)", array(
+			':NOME'=>$this->getNome(),
+			':SENHA'=>$this->getSenha()
+		));
+
+		if (isset($result[0])){
+			$this->setData($result[0]);
 		}
+	}
+/* Criando uma Procedure 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_usuarios_insert`(
+pnome varchar(64),
+psenha varchar(256)
+)
+BEGIN
+	insert into tbl_usuario (Nome,Senha) values (pnome, psenha); 
+    select * From tbl_usuarios where ID = Last_Insert_ID();
+END	*/
 
+	
+	public function __toString(){
+		return json_encode(array(
+			"ID"=>$this->getID(),
+			"Nome"=>$this->getNome(),
+			"Senha"=>$this->getSenha(),
+			"DataCadastro"=>$this->getDataCasdatro()->format("d/m/Y H:i:s")
+		));
+	}
 }
 
 ?>
--- index.php
+++ index.php
@@ -4,10 +4,42 @@ require_once ("config.php");
 
 
 
+ 
+//Usuario por Pk 
+//$usuarios = new Usuario();
+//$usuarios->obterPorPk(1);
+//echo $usuarios; // chama o metodo magico __toString e imprime 
+ 
+//Lista de Usuarios 
+//$lista =  Usuario:: listarUsuarios();
+//echo json_encode($lista); // é uma lista de objetos 
 
-$usuarios = new Usuario();
-$usuarios->obterPorPk(1);
+ 
+//carregar lista que contenha no nome ...
+//$search = Usuario::search("gui");
+//echo json_encode($search);
 
-echo $usuarios
+// carregar lista que contenha no
+//$login =  new Usuario();
+//$login -> login("Guilherme","1234");
+//echo login;
+
+//Incluir Usuários 
+//$usuario = new Usuario("Teste","12323%*");
+//$usuario->incluir();
+//echo $usuario;
+
+//Alterar Usuario 
+//$usuario = new Usuario();
+//$usuario->obterPorPk(16);
+//$usuario->alterar("Guilherme","9876");
+//echo $usuario;
+
+// DExcluir Usuario 
+$usuario = new Usuario();
+$usuario->obterPorPk(7);
+$usuario->excluir();
+
+echo $usuario;
 
 ?>
